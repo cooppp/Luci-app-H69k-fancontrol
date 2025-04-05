893aa93e9c12f623e44b4bbca530a0af88306bf1
cooppp <107824737+cooppp@users.noreply.github.com>
2025-04-05 11:13:22 +0800

    Create fan_ctrl.lua

---
+++ luci-app-fanctrl/files/usr/lib/lua/luci/controller/fan_ctrl.lua
@@ -0,0 +1,30 @@
+module("luci.controller.fan_ctrl", package.seeall)
+
+function index()
+    entry({"admin", "services", "fan_ctrl"}, cbi("fan_ctrl/basic"), _("Fan Control"), 60).dependent = true
+    entry({"admin", "services", "fan_ctrl", "status"}, call("action_status"))
+    entry({"admin", "services", "fan_ctrl", "control"}, call("action_control"))
+end
+
+function action_status()
+    local sys = require "luci.sys"
+    local util = require "luci.util"
+    local data = {
+        temp = util.trim(sys.exec("cat $(uci -q get fan_ctrl.settings.temp_sensor || echo '/sys/class/thermal/thermal_zone0/temp') | awk '{printf \"%.1f\", $1/1000}'")),
+        pwm = util.trim(sys.exec("cat $(uci -q get fan_ctrl.settings.pwm_ctrl || echo '/sys/class/hwmon/hwmon0/pwm1') 2>/dev/null")),
+        running = sys.call("pgrep fan_ctrl.sh >/dev/null") == 0
+    }
+    luci.http.prepare_content("application/json")
+    luci.http.write_json(data)
+end
+
+function action_control()
+    local cmd = luci.http.formvalue("cmd")
+    if cmd == "start" then
+        os.execute("/etc/init.d/fan_ctrl start >/dev/null 2>&1")
+    elseif cmd == "stop" then
+        os.execute("/etc/init.d/fan_ctrl stop >/dev/null 2>&1")
+    end
+    luci.http.prepare_content("application/json")
+    luci.http.write_json({success = true})
+end

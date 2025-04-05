298023ade71f24e53c3df89dd3c29beff6233a5b
cooppp <107824737+cooppp@users.noreply.github.com>
2025-04-05 20:28:05 +0800

    Update fanh69k.lua

--- luasrc/model/cbi/fanh69k.lua
+++ luasrc/model/cbi/fanh69k.lua
@@ -1,17 +1,26 @@
 local m, s, o
 
+-- 显式引入CBI模块
+require("luci.model.cbi")
+
+-- 初始化Map对象（强制校验配置文件）
 m = Map("fanh69k", translate("Fan Control Settings"), 
-    translate("Advanced PID-based thermal control for H69K devices"))
+    translate("Advanced PID-based thermal control for H69K devices"),
+    {on_init = function(self)
+        if not nixio.fs.access("/etc/config/fanh69k") then
+            luci.http.redirect(luci.dispatcher.build_url("admin/system/fanh69k/create_config"))
+        end
+    end})
 
 -- 启用开关
-s = m:section(NamedSection, "global", "control", translate("Global Switch"))
+s = m:section(NamedSection, "global", "control", "control", translate("Global Switch"))
 s.anonymous = true
 s:option(ListValue, "enabled", translate("Enable Control"))
     :value("1", translate("Enabled"))
     :value("0", translate("Disabled"))
 
 -- 基础参数（依赖启用状态）
-s = m:section(NamedSection, "global", "control", translate("Basic Parameters"))
+s = m:section(NamedSection, "global", "control", "control", translate("Basic Parameters"))
 s.anonymous = true
 s:depends("enabled", "1")
 
